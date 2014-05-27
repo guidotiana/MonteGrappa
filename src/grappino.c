923d1664382c674d014864e4be98ffb65969639d
Riccardo Capelli <riccardo.capelli@outlook.com>
2014-05-27 20:35:20 +0200

    fix indentation and (some) comments in grappino.c

--- src/grappino.c
+++ src/grappino.c
@@ -10,16 +10,19 @@
 
 int main(int argc, char *argv[])
 {
-	struct s_parms *p;
-	struct atom_s *pdb;
-	struct s_polymer *polymer;
-	int npdb,nchain,i,nrot_kinds=0,ntypes=0,nat,nbackmax;
-	struct rot_input_s *rotamers;
-	struct s_potential *u;
-	double **cm;
+	struct s_parms *p;                      // parameters structure
+	struct atom_s *pdb;                     // atom structure
+	struct s_polymer *polymer;              // polymer structure
+	int npdb, nchain, i, nrot_kinds=0, ntypes=0, nat, nbackmax;
+	struct rot_input_s *rotamers;           // rotamers structure
+	struct s_potential *u;                  // potential structure
+	double **cm;                            // ???
 	FILE *fin;
 
-	if (argc != 2) { fprintf(stderr,"usage: grappino file.in\n"); exit(1); }
+	if (argc != 2) {
+        fprintf(stderr,"usage: grappino file.in\n");
+        exit(1);
+    }
 	
 	// open input file
 	fin = fopen(argv[1],"r");
@@ -31,10 +34,10 @@ int main(int argc, char *argv[])
 	Parse(fin,p);
 	fclose(fin);
 
-	// get pdb
-	fprintf(stderr,"Getting pdb\n");
+	// get pdb and save the number of atoms (npdb)
+	fprintf(stderr,"Getting pdb...\n");
 	pdb = AlloAtoms(NATOMMAX);
-	npdb = ReadPDB(pdb,p->pdbfile,p->hydrogens,&nchain,&nbackmax,p);			//npdb is the total number of atoms
+	npdb = ReadPDB(pdb,p->pdbfile,p->hydrogens,&nchain,&nbackmax,p);
 
 	// load rotamers
 	if (p->rotamers)
@@ -50,20 +53,20 @@ int main(int argc, char *argv[])
 	// POLYMER FILE
 	///////////////
 
-	// allocate stuff
+	// allocate polymer and tables
 	fprintf(stderr,"Allocate polymer\n");
 	polymer = AlloPolymer(nchain,nbackmax,NSIDEMAX,NROTMAX,npdb,0,0,stderr);
 	polymer->tables = InitTables(stderr);
 	for (i=1;i<nchain;i++) (polymer+i)->tables = polymer->tables;			// tables of all polymers point to same address
 
 	// transfer pdb to polymer structure
-	nat = Pdb2Polymer(pdb,nchain,npdb,polymer,p,rotamers,nrot_kinds);		//nat is the total number of atoms
+	nat = Pdb2Polymer(pdb,nchain,npdb,polymer,p,rotamers,nrot_kinds);		// nat is the total number of atoms
 
 	/////////////////
 	// POTENTIAL FILE
 	/////////////////
 
-	// set types
+	// set atomtypes
 	if (!strcmp(p->atomtypes,"go")) ntypes = SetGoTypes(polymer,nchain,npdb);
 	else ntypes = ReadTypes(polymer,nchain,npdb,p->atomtypes);
 	
@@ -74,30 +77,35 @@ int main(int argc, char *argv[])
 	u->g_imin = p->imin;
 	fprintf(stderr,"imin = %d\n",u->g_imin);
 
-	// go potential
-	if (!strcmp(p->potential,"go"))
-	{
+	// define go potential
+	if (!strcmp(p->potential,"go")) {
 		Go_Pairs(p,polymer,u->e,u->r_2,u->r0_2,nchain,ntypes,cm);
 		u->g_r0hard = p->rhard;
 	}
 
-	if(p->splice)
-	{
+	if (p->splice) {
 		u->splice=1;
 		u->ke_splice = p->ke_splice;
-		u->kr2_splice = p->kr_splice;	//warning: here kr2_splice is not squared, it's gonna be squared in montegrappa!
+		u->kr2_splice = p->kr_splice;	// warning: here kr2_splice is not squared, it's gonna be squared in montegrappa!
 	}
 
-	// cysteines
+	// cysteine bridge
+    // NON E' POSSIBLE LEGGERLO DAL PDB, NELLA PARTE "BONDS" IN FONDO?
 	if (p->cys<-EPSILON || p->cys>EPSILON)
 		DisulphideBonds(p,polymer,u->e,u->r_2,u->r0_2,nchain,ntypes);
 
 
 	// global energy terms
-	if (p->e_homo<-EPSILON || p->e_homo> EPSILON) { u->g_ehomo = p->e_homo; u->g_rhomo = p->r_homo; }
-
-	if (p->go_dih) Go_Dihedrals(p,polymer,nchain,u->dih01,u->dih03,u);
-	if (p->go_ang) Go_Angles(p,polymer,nchain,u->ang0,u);
+	if (p->e_homo<-EPSILON || p->e_homo> EPSILON) {
+        u->g_ehomo = p->e_homo;
+        u->g_rhomo = p->r_homo;
+    }
+    
+    // go dihedrals and go angles
+	if (p->go_dih)
+        Go_Dihedrals(p,polymer,nchain,u->dih01,u->dih03,u);
+	if (p->go_ang)
+        Go_Angles(p,polymer,nchain,u->ang0,u);
 
 
 
@@ -107,7 +115,8 @@ int main(int argc, char *argv[])
 	AppendPotentialComments(p, p->eoutfile);
 
 	//if you want a file.op to optimize the potential 
-	if(strcmp(p->op_file,"")) PrintOpGoFile(p->op_file,cm,polymer,nchain,p->op_kind);
+	if(strcmp(p->op_file,""))
+        PrintOpGoFile(p->op_file,cm,polymer,nchain,p->op_kind);
 
 	exit(0);
 }
@@ -118,7 +127,7 @@ void Parse(FILE *fp, struct s_parms *p)
 	char aux[500];
 	int i,j,sum = 0;
 
-	// defaults
+	// Set default values
 	p->hydrogens = 0;
 	p->rnat = 3.5;
 	p->rhard = 2.;
@@ -132,7 +141,9 @@ void Parse(FILE *fp, struct s_parms *p)
 	strcpy(p->eoutfile,"potential.pot");
 	strcpy(p->cntfile,"");
 	p->n_back_a = 3;
-	strcpy(p->back_a[0],"N"); strcpy(p->back_a[1],"CA"); strcpy(p->back_a[2],"C");
+	strcpy(p->back_a[0],"N");
+    strcpy(p->back_a[1],"CA");
+    strcpy(p->back_a[2],"C");
 	for(i=0;i<100;i++) p->move_a[i]=1;
 	p->debug=0;
 	p->rotamers=0;
@@ -148,7 +159,8 @@ void Parse(FILE *fp, struct s_parms *p)
 	p->sidescratch = 0;
 	strcpy(p->op_file,"");
 	strcpy(p->op_kind,"GO_DIST_CA");
-
+    
+    // read parameters file
 	while(fgets(aux,500,fp)!=NULL)
     {
 		ReadParS(aux,"pdbfile",p->pdbfile);
@@ -189,76 +201,77 @@ void Parse(FILE *fp, struct s_parms *p)
 		ReadParS(aux,"op_file",p->op_file);
 		ReadParS(aux,"op_kind",p->op_kind);
 
-		if (!strncmp(aux,"backbone_atoms",14))
-		  {
-			  (p->n_back_a)=0;
-			  ReadParD(aux,"backbone_atoms",&(p->n_back_a));
+        // read explicitly declared backbone atoms
+		if (!strncmp(aux,"backbone_atoms",14)) {
+            (p->n_back_a)=0;
+            ReadParD(aux,"backbone_atoms",&(p->n_back_a));
+            if ( sscanf(aux,"backbone_atoms %*d %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s",
+					  p->back_a[0],p->back_a[1],p->back_a[2],p->back_a[3],p->back_a[4],p->back_a[5],
+                      p->back_a[6],p->back_a[7],p->back_a[8],p->back_a[9],p->back_a[10],p->back_a[11],
+                      p->back_a[12],p->back_a[13],p->back_a[14],p->back_a[15],p->back_a[16],p->back_a[17],
+                      p->back_a[18],p->back_a[19])<1 ) Error("Cannot read backbone_atoms in inputfile");
+            // backbone atoms manually entered are unlocked by default,
+            // one must specify if they need to be locked
+            for(i=0;i<p->n_back_a;i++) p->move_a[i]=1;
+        }
 
 
-					if ( sscanf(aux,"backbone_atoms %*d %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s",
-					  p->back_a[0],p->back_a[1],p->back_a[2],p->back_a[3],p->back_a[4],p->back_a[5],p->back_a[6],p->back_a[7],
-					  p->back_a[8],p->back_a[9],p->back_a[10],p->back_a[11],p->back_a[12],p->back_a[13],p->back_a[14],p->back_a[15],
-					  p->back_a[16],p->back_a[17],p->back_a[18],p->back_a[19])<1) Error("Cannot read backbone_atoms in inputfile");
-					for(i=0;i<p->n_back_a;i++) p->move_a[i]=1;   		//backbone atoms manually entered are unlocked by default, one must specify if they need to be locked
-		  }
+        //locked atoms: only backbone atoms can be locked, allowed moves of side chain atoms are in rotamers libraries
+        if (!strncmp(aux,"locked_atoms",12)) {
+            char locked_a[100][100];
+            if ( sscanf(aux,"locked_atoms %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s",
+                locked_a[0],locked_a[1],locked_a[2],locked_a[3],locked_a[4],locked_a[5],
+                locked_a[6],locked_a[7], locked_a[8],locked_a[9],locked_a[10],locked_a[11],
+                locked_a[12],locked_a[13],locked_a[14],locked_a[15],locked_a[16],locked_a[17],
+                locked_a[18],locked_a[19])>=1 ) {
+                    for (j=0;j<(p->n_back_a);j++)
+                        for (i=0;i<(p->n_back_a);i++)
+                            if (!strcmp(locked_a[j],p->back_a[i])) p->move_a[i]=0;
+            }
+            else Error("Cannot read locked_atoms in inputfile");
+        }
 
-
-		  	  //locked atoms: only backbone atoms can be locked, allowed moves of side chain atoms are in rotamers libraries
-				if (!strncmp(aux,"locked_atoms",12))
-				{
-					char locked_a[100][100];
-
-
-					if ( sscanf(aux,"locked_atoms %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s",
-						locked_a[0],locked_a[1],locked_a[2],locked_a[3],locked_a[4],locked_a[5],locked_a[6],locked_a[7],
-						locked_a[8],locked_a[9],locked_a[10],locked_a[11],locked_a[12],locked_a[13],locked_a[14],locked_a[15],
-						locked_a[16],locked_a[17],locked_a[18],locked_a[19])>=1)
-					{
-
-						for (j=0;j<(p->n_back_a);j++)
-							for (i=0;i<(p->n_back_a);i++)
-								if (!strcmp(locked_a[j],p->back_a[i])) p->move_a[i]=0;
-					}
-					else Error("Cannot read locked_atoms in inputfile");
-				}
-
-			//splice option in potential
-			if (!strncmp(aux,"splice",6))
-			{
-			p->splice=1;
-			if ( sscanf(aux,"splice %lf %lf",&(p->kr_splice), &(p->ke_splice)) < 1 ) Error("Cannot read splice option in inputfile (splice <double> <double>)");
-			}
-
-
+        //splice option in potential
+		if (!strncmp(aux,"splice",6)) {
+            p->splice=1;
+            if ( sscanf(aux,"splice %lf %lf",&(p->kr_splice), &(p->ke_splice)) < 1 )
+                Error("Cannot read splice option in inputfile (splice <double> <double>)");
+        }
 
     }
-
+    
+    // print backbone atoms
 	fprintf(stderr, "%d backbone atoms found. They are: ",p->n_back_a);
-					  for (i=0;i<(p->n_back_a);i++)   fprintf(stderr, "	%s",p->back_a[i]);
-		fprintf(stderr, "\n these backbone atoms are locked: ");
-					  for (i=0;i<(p->n_back_a);i++)
-						  if(p->move_a[i]==0) fprintf(stderr, "	%s",p->back_a[i]);
+	for (i=0;i<(p->n_back_a);i++)
+        fprintf(stderr, "%s",p->back_a[i]);
+    // print locked atoms
+    fprintf(stderr, "\n these backbone atoms are locked: ");
+	for (i=0;i<(p->n_back_a);i++)
+        if (p->move_a[i]==0) fprintf(stderr, "	%s",p->back_a[i]);
+    
+	for (j=0;j<(p->n_back_a);j++)
+        sum += p->move_a[j];
+    if (sum==p->n_back_a)
+        fprintf(stderr,"\n\n** WARNING: all backbone atoms are UNLOCKED ==> EVERYTHING can be MOVED\n"
+                "** remember that the keyword locked_atoms must be written below backbone_atoms not above!!\n");
+    if (sum==0)
+        fprintf(stderr,"\n\n** WARNING: all backbone atoms are LOCKED ==> NOTHING can be MOVED\n");
 
-	for (j=0;j<(p->n_back_a);j++) sum += p->move_a[j];
-	 if(sum==p->n_back_a) fprintf(stderr,"\n\n** WARNING: all backbone atoms are UNLOCKED ==> EVERYTHING can be MOVED\n"
-			 	 	 	 	 	 	 	 	 "** remember that the keyword locked_atoms must be written below backbone_atoms not above!!\n");
-	 if(sum==0) fprintf(stderr,"\n\n** WARNING: all backbone atoms are LOCKED ==> NOTHING can be MOVED\n");
-
-	 if(p->splice)
-	 {
-		 fprintf(stderr,"\n splice option enabled with kr_splice = %lf\t ke_splice = %lf\n",p->kr_splice,p->ke_splice);
-		 if(p->kr_splice>1) Error("in spice option kr_splice can't be bigger than 1\n");
-		 if(p->ke_splice>1) fprintf(stderr,"WARNING: in splice option ke_splice > 1 sounds strange, it makes long range interactions stronger than short range ones!!\n");
-		 if(p->use_nativedist && p->kr_splice*p->k_native_r*p->rnat< p->k_native_hc*p->rnat) fprintf(stderr,"\n ** WARNING **: kr_splice*k_native_r*r_native < k_native_hc*r_native ==> spliced distance smaller than hardcore repulsion!!\n");
-		 	 fprintf(stderr,"# splice option enabled. Size of the potential well:\n\n");
-			 fprintf(stderr,"# E \t |\n");
-			 fprintf(stderr,"# | \t |\t\t__________ 0\n");
-			 fprintf(stderr,"# | \t |\t\t|\n");
-			 fprintf(stderr,"# | \t |\t________| %4.2lf *E_native\n",p->ke_splice);
-			 fprintf(stderr,"# | \t |\t|\n");
-			 fprintf(stderr,"# | \t |______| E_native\n");
-			 fprintf(stderr,"# |______________________________> r\n");
-			 fprintf(stderr,"# 0 \t%4.2lf\t%4.2lf\t%4.2lf\n\n",p->k_native_hc*p->rnat,p->kr_splice*p->k_native_r*p->rnat,p->k_native_r*p->rnat);
+    if (p->splice) {
+        fprintf(stderr,"\n splice option enabled with kr_splice = %lf\t ke_splice = %lf\n",p->kr_splice,p->ke_splice);
+        if (p->kr_splice>1)
+        Error("in spice option kr_splice can't be bigger than 1\n");
+		if (p->ke_splice>1) fprintf(stderr,"WARNING: in splice option ke_splice > 1 sounds strange, it makes long range interactions stronger than short range ones!!\n");
+		if (p->use_nativedist && p->kr_splice*p->k_native_r*p->rnat< p->k_native_hc*p->rnat) fprintf(stderr,"\n ** WARNING **: kr_splice*k_native_r*r_native < k_native_hc*r_native ==> spliced distance smaller than hardcore repulsion!!\n");
+            fprintf(stderr,"# splice option enabled. Size of the potential well:\n\n");
+			fprintf(stderr,"# E \t |\n");
+			fprintf(stderr,"# | \t |\t\t__________ 0\n");
+			fprintf(stderr,"# | \t |\t\t|\n");
+			fprintf(stderr,"# | \t |\t________| %4.2lf *E_native\n",p->ke_splice);
+			fprintf(stderr,"# | \t |\t|\n");
+			fprintf(stderr,"# | \t |______| E_native\n");
+			fprintf(stderr,"# |______________________________> r\n");
+			fprintf(stderr,"# 0 \t%4.2lf\t%4.2lf\t%4.2lf\n\n",p->k_native_hc*p->rnat,p->kr_splice*p->k_native_r*p->rnat,p->k_native_r*p->rnat);
 	 }
 
 	 fprintf(stderr, "\n \n");
